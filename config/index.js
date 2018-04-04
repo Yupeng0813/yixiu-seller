b2d19bc8d1dbf497407a027b587cf0ced853eb67
Yupeng0813 <1072172744@qq.com>
2018-04-04 16:25:34 +0800

    q

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'yixiuseller',
-    assetsPublicPath: '',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
