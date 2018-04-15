33ccc813204bc9f76c4bf4677e845814ad9e0c15
popinstyle <35994859@qq.com>
2018-04-16 00:26:27 +0800

    测试

--- src/views/sellerHome/pages/payBail.vue
+++ src/views/sellerHome/pages/payBail.vue
@@ -185,7 +185,7 @@ export default {
                           if (update.code == 200) {
                             let newShop = await that.$api.sendData('https://m.yixiutech.com/sql/find', {
                               collection: 'Shop',
-                              shop: payInfo.shop
+                              _id: payInfo.shop
                             })
                             sessionStorage.setItem('shopData', JSON.stringify(newShop.data));
                             that.$router.push("/sellerHome");
