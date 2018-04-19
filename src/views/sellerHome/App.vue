c98a070436a0d80772db4a93b17ac09f4ab67b95
popinstyle <35994859@qq.com>
2018-04-20 00:39:08 +0800

    修改

--- src/views/sellerHome/App.vue
+++ src/views/sellerHome/App.vue
@@ -88,8 +88,8 @@
 		// },
 		// 删除店铺信息  慎用
 		async created () {
-			let res = await this.$api.sendData('https://m.yixiutech.com/shop/filter', {limit: 100, skip: 0});
-			sessionStorage.setItem('shopData', JSON.stringify(res.data[12]));
+			// let res = await this.$api.sendData('https://m.yixiutech.com/shop/filter', {limit: 100, skip: 0});
+			// sessionStorage.setItem('shopData', JSON.stringify(res.data[12]));
 
 			 // let ss = await this.$api.sendData('https://m.yixiutech.com/sql/find', {
 			 // 	collection: 'Shop',
